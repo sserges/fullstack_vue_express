2ad0e84415c81e1bef71c6e3919b873a23f7e406
sserges <agounkeserge@gmail.com>
2020-09-29 20:23:00 +0000

    delete post route added

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
 
 // Get Posts
-router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    res.send(await posts.find({}).toArray());
+router.get("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
-// Add Post 
-router.post('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    await posts.insertOne({
-        text: req.body.text,
-        createdAt: new Date()
-    });
+// Add Post
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
 
-    res.status(201).send();
+  res.status(201).send();
+});
+
+// Delete Post
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
 });
 
 async function loadPostsCollection() {
-    const client = await mongodb.MongoClient.connect(
-        'mongodb+srv://nemo:iQWoQmaawImqiYH3@cluster0.p34fz.mongodb.net/vue_express?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    );
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://nemo:iQWoQmaawImqiYH3@cluster0.p34fz.mongodb.net/vue_express?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
-    return client.db('vue_express').collection('posts');
+  return client.db("vue_express").collection("posts");
 }
 
 module.exports = router;
