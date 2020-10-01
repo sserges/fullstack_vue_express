05086eb9d26794df48dd64d8d8da6105264dcb1b
sserges <agounkeserge@gmail.com>
2020-10-01 11:05:38 +0000

    CreatePost method added

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -5,19 +5,19 @@ const url = 'http://localhost:5000/api/posts/';
 class PostService {
   // Get Posts
   static getPosts() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const res = await axios.get(url);
-        const data = res.data;
-        resolve(
-          data.map((post) => ({
-            ...post,
-            createdAt: new Date(post.createdAt),
-          }))
-        );
-      } catch (error) {
-        reject(error);
-      }
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then((res) => {
+          const data = res.data;
+          resolve(
+            data.map((post) => ({
+              ...post,
+              createdAt: new Date(post.createdAt),
+            }))
+          );
+        })
+        .catch((error) => reject(error));
     });
   }
 
